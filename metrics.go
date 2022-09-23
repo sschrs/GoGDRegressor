7fbd8707720cc11679bd15fb7ad288c104f31051
sschrs <s_ozarslan07@outlook.com>
2022-09-23 14:57:37 +0300

    RMSE function created

--- metrics.go
+++ metrics.go
@@ -1,6 +1,9 @@
 package GDRegressor
 
-import "github.com/sschrs/matrix"
+import (
+	"github.com/sschrs/matrix"
+	"math"
+)
 
 func MSE(yReal, yPredicted [][]float64) float64 {
 	real := matrix.AsMatrix(yReal)
@@ -9,4 +12,8 @@ func MSE(yReal, yPredicted [][]float64) float64 {
 	return real.Subtract(predicted).Apply(func(x matrix.Col) matrix.Col {
 		return x * x
 	}).Mean()
+}
+
+func RMSE(yReal, yPredicted [][]float64) float64 {
+	return math.Sqrt(MSE(yReal, yPredicted))
 }
