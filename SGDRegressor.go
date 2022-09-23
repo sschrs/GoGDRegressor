17fff66b795ba847a65a49a260fc598d7cc59b27
sschrs <s_ozarslan07@outlook.com>
2022-09-23 17:54:50 +0300

    Fit function is written for stochastic gradient descent

--- SGDRegressor.go
+++ SGDRegressor.go
@@ -1,6 +1,10 @@
 package GDRegressor
 
-import "github.com/sschrs/matrix"
+import (
+	"github.com/sschrs/matrix"
+	"math/rand"
+	"time"
+)
 
 type SGDRegressor struct {
 	Iterations     int
@@ -8,4 +12,47 @@ type SGDRegressor struct {
 	coef           []float64
 	fitted         bool
 	theta, x, y    matrix.Matrix
+}
+
+func NewSGDRegressor() *SGDRegressor {
+	return &SGDRegressor{
+		Iterations: 1000,
+		Eta:        0.1,
+		fitted:     false,
+	}
+}
+
+func learning_schedule(eta float64) float64 {
+	return 5 / (50 + eta)
+}
+
+func (sgd *SGDRegressor) Fit(x, y [][]float64) *SGDRegressor {
+	X := matrix.AsMatrix(x)
+	Y := matrix.AsMatrix(y)
+
+	X = X.JoinColumn(matrix.GenerateColumn(len(x), 1), 0)
+
+	sgd.theta = matrix.GenerateRand(X.Shape()["cols"], 1).Divide(10)
+
+	for i := 0; i < sgd.Iterations; i++ {
+		for j := 0; j < len(sgd.x); j++ {
+			rand.Seed(time.Now().UnixNano())
+			random_index := rand.Intn(len(sgd.x))
+			xi := X[random_index : random_index+1]
+			yi := Y[random_index : random_index+1]
+
+			gradient := xi.T().Dot(xi.Dot(sgd.theta).Subtract(yi)).Multiply(2)
+			sgd.Eta = learning_schedule(sgd.Eta)
+			sgd.theta = sgd.theta.Subtract(gradient.Multiply(sgd.Eta))
+		}
+	}
+	sgd.fitted = true
+	sgd.intercept = float64(sgd.theta[0][0])
+
+	var coef []float64
+	for i := 1; i < len(sgd.theta); i++ {
+		coef = append(coef, float64(sgd.theta[i][0]))
+	}
+
+	return sgd
 }
